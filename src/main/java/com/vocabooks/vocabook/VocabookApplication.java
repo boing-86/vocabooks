b050a51d5b076b9f4e9d9286e4c2843a47786fa6
boin.jeongi <boing0806@gmail.com>
2026-06-18 10:29:46 +0900

    initial commit

---
+++ src/main/java/com/vocabooks/vocabook/VocabookApplication.java
@@ -0,0 +1,13 @@
+package com.vocabooks.vocabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vocabooks/vocabook/VocabookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vocabooks.vocabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
